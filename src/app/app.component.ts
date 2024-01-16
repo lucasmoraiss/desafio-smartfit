4d8c5515fb3e637dfcae0db215445c641092d754
lucasmoraiss <lucasmoraiss122@outlook.com>
2024-01-16 14:26:15 -0300

    feature: forms creating and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { FormsComponent } from "./components/forms/forms.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent]
 })
 export class AppComponent {
   title = 'desafio-smartfit';
