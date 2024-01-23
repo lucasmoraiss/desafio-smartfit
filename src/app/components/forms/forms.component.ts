0ae3de435bc351aef0a72cb914664dc8d239ca9e
lucasmoraiss <lucasmoraiss122@outlook.com>
2024-01-23 17:40:53 -0300

    feature: add form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss'
+  styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
   results = [];
+  formGroup!: FormGroup;
+
+  onSubmit(): void{
+    console.log(this.formGroup.value)
+  }
+
+  onClear(): void{
+    this.formGroup.reset();
+  }
+
+  constructor(private formBuilder: FormBuilder){
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    })
+  }
 }
